3ed485aab5bca36820cdeaaf0b4571346cdc4528
Ranie Santos <ranietots32@gmail.com>
2017-04-05 11:47:04 +0800

    used .env file to store personal data

---
+++ config/me.php
@@ -0,0 +1,70 @@
+<?php
+
+return [
+
+	//-------------------------------------------------------------------------
+	// Basic Info
+	//-------------------------------------------------------------------------
+	'fullname' => 'Ranie Rodriguez Santos',
+	'address' => env('MY_ADDRESS', ''),
+	'email' => env('MY_EMAIL', ''),
+	'website' => 'http://raniesantos.github.io',
+	'mobile' => env('MY_MOBILE', ''),
+	'photo' => 'img/my-photo.png',
+
+	//-------------------------------------------------------------------------
+	// Skills
+	//-------------------------------------------------------------------------
+	'languages' => [
+		'HTML',
+		'CSS',
+		'JavaScript',
+		'PHP',
+		'MySQL',
+	],
+	'web_techs' => [
+		'Sass (CSS Preprocessor)',
+		'jQuery (JavaScript Library)',
+		'Bootstrap and Foundation (Responsive Front-End Frameworks)',
+		'Laravel (PHP MVC Framework)',
+		'Gulp (Task runner)',
+		'npm/Yarn for JavaScript and Composer for PHP (Package managers)',
+	],
+	'concepts' => [
+		'Responsive web design',
+		'MVC architectural pattern',
+	],
+
+	//-------------------------------------------------------------------------
+	// Work Experience
+	//-------------------------------------------------------------------------
+	'jobs' => [
+		'',
+		'',
+	],
+
+	//-------------------------------------------------------------------------
+	// Education
+	//-------------------------------------------------------------------------
+	'schools' => [
+		'',
+		'',
+	],
+
+	//-------------------------------------------------------------------------
+	// Seminars
+	//-------------------------------------------------------------------------
+	'seminars' => [
+		'',
+		'',
+	],
+
+	//-------------------------------------------------------------------------
+	// Achievements
+	//-------------------------------------------------------------------------
+	'achievements' => [
+		'Received 3rd Place JRU Web Developer of the Year award in 2015.',
+		'Passed Professional Level Civil Service Exam in October 2016.',
+	],
+
+];
