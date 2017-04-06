c1de24ee7e1e1c10b4a79819b655c7959505b658
Ranie Santos <ranietots32@gmail.com>
2017-04-07 00:41:58 +0800

    moved scss files

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,4 @@
 const { mix } = require('laravel-mix');
 
-mix.sass('resources/assets/sass/resume.scss', 'public/css');
+mix.sass('resources/assets/sass/resume/resume.scss', 'public/css')
+	.sass('resources/assets/sass/app.scss', 'public/css');
