8c69d4315e4f97d068ed02f9f61789f9586fcf7c
Ranie Santos <ranietots32@gmail.com>
2017-05-08 02:03:45 +0800

    redesigned buttons on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,20 @@
 @extends('layouts.base')
 
 @section('content')
+
 	<header>{{ config('app.name') }}</header>
 
-	<a class="pdf" target="_blank" href="{{ url('/preview/pdf') }}">View as PDF</a>
-	<a class="web" target="_blank" href="{{ url('/preview/web') }}">View as web page</a>
-	<a class="download" href="{{ url('/download') }}">Download ZIP</a>
+	<a class="pdf" href="{{ url('/preview/pdf') }}" target="_blank">
+		<span class="fa fa-file-pdf-o"></span>
+		View as PDF file
+	</a>
+	<a class="web" href="{{ url('/preview/web') }}" target="_blank">
+		<span class="fa fa-code"></span>
+		View as web page
+	</a>
+	<a class="download" href="{{ url('/download') }}">
+		<span class="fa fa-download"></span>
+		Download ZIP file
+	</a>
+
 @stop
