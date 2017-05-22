0fb7854af990782d9cb767e12a2c98adc03c42b4
Ranie Santos <ranietots32@gmail.com>
2017-05-23 04:04:24 +0800

    changed file directories, added checklist text file

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -11,17 +11,18 @@ class DownloadController extends Controller
 	public function download() {
 		// public, no mobile number
 		PDF::loadView('resume', ['mobile' => false])
-			->save($pdfA = 'pdf/ranie-santos-resume-a.pdf');
+			->save($pdfA = 'files/ranie-santos-resume-a.pdf');
 
 		// private, has mobile number
 		PDF::loadView('resume', ['mobile' => true])
-			->save($pdfB = 'pdf/ranie-santos-resume-b.pdf');
+			->save($pdfB = 'files/ranie-santos-resume-b.pdf');
 
 		$zipFilename = 'resume-' . strtolower(date('M.d.Y-H.i.s')) . '.zip';
 
-		Zipper::make($zip = public_path('zip/' . $zipFilename))->add([
+		Zipper::make($zip = public_path('files/zip/' . $zipFilename))->add([
 			public_path($pdfA),
 			public_path($pdfB),
+			public_path('files/checklist.txt'),
 		])->close();
 
 		return response()
