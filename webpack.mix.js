034a41bb1281b71c950570a1ad48a266a22cc7f7
Ranie Santos <ranietots32@gmail.com>
2017-08-28 12:29:33 +0800

    minor change to mix file

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,4 @@ mix.sass('resources/assets/sass/resume/resume-pdf.scss', 'public/css')
 	.sass('resources/assets/sass/resume/resume-web.scss', 'public/css')
 	.sass('resources/assets/sass/app.scss', 'public/css');
 
-mix.browserSync('demopdf.app');
+mix.browserSync('rezugenerator.dev');
