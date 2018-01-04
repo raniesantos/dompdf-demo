ea1adff91e71b6bff2894b66c8e6433aea23b65f
Ranie Santos <ranietots32@gmail.com>
2018-01-04 14:55:23 +0800

    Changed files watched by laravel-mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,14 @@ mix.sass('resources/assets/sass/resume/resume-pdf.scss', 'public/css')
     .sass('resources/assets/sass/resume/resume-web.scss', 'public/css')
     .sass('resources/assets/sass/app.scss', 'public/css');
 
-mix.browserSync('rezugenerator.test');
+mix.browserSync({
+    proxy: 'rezugenerator.test',
+    files: [
+        'app/**/*.php',
+        'resources/views/**/*.php',
+        'public/js/**/*.js',
+        'public/css/**/*.css',
+        '.env',
+        'config/resume.php'
+    ]
+});
